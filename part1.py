6beff421a4bca1ac67c9c39a199fa78225970827
Damian Vincent Dobrowolski <>
2025-10-08 19:54:30 -0500

    adds retry functionality (use tiny timeout to test)

--- part1.py
+++ part1.py
@@ -1,12 +1,18 @@
 import socket
 import threading
 
+LISTEN_IP = '127.0.0.1'
 PROXY_PORT = 1053
 DEFAULT_UPSTREAM_DNS = "8.8.8.8"
+DNS_PORT = 53
+
+# timeout params
+UPSTREAM_TIMEOUT = 2.0
+RETRIES = 3                
 
 def run_proxy():
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.bind(('', PROXY_PORT))
+    sock.bind((LISTEN_IP, PROXY_PORT))
     print(f"DNS proxy listening on port: {PROXY_PORT}")
 
     while True:
@@ -22,29 +28,28 @@ def handle_dns_request(data, addr, sock):
     
     try:
         upstream_dns_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        upstream_dns_socket.settimeout(UPSTREAM_TIMEOUT)
         response = None
 
-        attempt = 0
-        while attempt < 3:
+        for attempt in range(RETRIES):
             try:
-                upstream_dns_socket.sendto(data, (DEFAULT_UPSTREAM_DNS, 53))
+                upstream_dns_socket.sendto(data, (DEFAULT_UPSTREAM_DNS, DNS_PORT))
                 response, _ = upstream_dns_socket.recvfrom(2048)
                 break
-            
+            except socket.timeout:
+                print(f"Upstream timeout (attempt {attempt+1}/{RETRIES})")
             except Exception as e:
-                attempt += 1
-                if attempt == 3:
-                    print(f"Failed to get response from upstream DNS after 3 attempts: {e}")
-                    response = 'ERROR: Upstream DNS unreachable'.encode()
+                print(f"Upstream error on attempt {attempt+1}/{RETRIES}: {e}")
+                response = 'ERROR: Upstream DNS unreachable'.encode()
 
         sock.sendto(response, addr)
         print(f"Sent response back to {addr}")
-        upstream_dns_socket.close()
-
+    
     except Exception as e:
         print(f"Error handling DNS request in thread from {addr}: {e}")
-
- 
+    finally:
+        if upstream_dns_socket:
+            upstream_dns_socket.close()
 
 if __name__ == "__main__":
     run_proxy()
